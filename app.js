df611749293cd726c5d6fd7a2a179f311c65656e
profCodey <otunaiyademilade@gmail.com>
2022-10-30 21:54:24 +0100

    update

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-const http = require("http");
 
-const host = "localhost";
-const port = 8080;
+
+
+const express = require('express');
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 8080;
+
+app.use(cors())
+
+
 
 const data = {
   slackUsername: "Demilade Otunaiya",
@@ -10,16 +17,13 @@ const data = {
   bio: "Software Engineer with over three years of experience in full-stack development and leading product cycle from conception to completion. Guided a team of 5-15 members through 5+ product launches at a recent experience in a high-growth technology startup. Passionate about building products that solve real-world problems and creating a positive impact on the world.",
 };
 
-const requestListener = function (req, res) {
-  res.setHeader("Content-Type", "application/json");
-  res.writeHead(200);
-  res.end(JSON.stringify(data, null, 3));
-};
+app.get('/', (req, res) => {
+    res.json({
+            data
+    })
+})  
 
-const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-  console.log(`Server is running on http://${host}:${port}`);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
 });
-
-
-
